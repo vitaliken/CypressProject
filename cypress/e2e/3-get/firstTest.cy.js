f720e75e437d8574d80a36de7aab95b05c24082c
Vitaly Hutarau <vitaliken@gmail.com>
2022-09-07 15:33:28 +0300

    Search for elements: Find, Eq

--- cypress/e2e/3-get/firstTest.cy.js
+++ cypress/e2e/3-get/firstTest.cy.js
@@ -1,34 +1,51 @@
-it('By ID', () => {
-    cy.visit('https://facebook.com/');
-    cy.get('#email');
+/// <reference types="Cypress" />
+
+
+// it('By ID', () => {
+//     cy.visit('https://facebook.com/');
+//     cy.get('#email');
+// })
+
+// it('By Class', () => {
+//     cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax');
+//     cy.get('.DocSearch');
+// })
+
+// it('By Tag', () => {
+//     cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax');
+//     cy.get('nav');
+// })
+
+// it('By Tag value', () => {
+//     cy.visit('https://facebook.com/');
+//     cy.get('[name="pass"]');
+// })
+
+// it('By Diff Tag', () => {
+//     cy.visit('https://facebook.com/');
+//     cy.get('[data-testid="open-registration-form-button"][role="button"]');
+// })
+
+// it('By Diff Types', () => {
+//     cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax');
+//     cy.get('button[type="button"][aria-label="Search"]');
+// })
+
+// it('By contains name', () => {
+//     cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax');
+//     cy.get('*[class^="max"]');
+// })
+
+
+
+
+
+it('Using Get with Find and Eq', () => {
+    cy.visit('https://amihome.by/catalog/upholstered/soft_corners/');
+    cy.get('.bottom-header').find('ul').find('li').eq('2');
 })
 
-it('By Class', () => {
-    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax');
-    cy.get('.DocSearch');
-})
-
-it('By Tag', () => {
-    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax');
-    cy.get('nav');
-})
-
-it('By Tag value', () => {
-    cy.visit('https://facebook.com/');
-    cy.get('[name="pass"]');
-})
-
-it('By Diff Tag', () => {
-    cy.visit('https://facebook.com/');
-    cy.get('[data-testid="open-registration-form-button"][role="button"]');
-})
-
-it('By Diff Types', () => {
-    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax');
-    cy.get('button[type="button"][aria-label="Search"]');
-})
-
-it.only('By contains name', () => {
-    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax');
-    cy.get('*[class^="max"]');
+it.only('Using Get with Find and Eq 2', () => {
+    cy.visit('https://docs.cypress.io/api/table-of-contents');
+    cy.get('aside').find('div').find('button').eq('4').click();
 })
